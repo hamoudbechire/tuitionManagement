08cc78e3523228c627a1f31b851705a4e3bdd13d
Aynin <amelayni@gmail.com>
2022-03-31 12:24:09 +0200

    creation pages & services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ProfesseurPipe } from './pages/professeur.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProfesseurPipe],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
---
+++ src/app/pages/professeur.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ProfesseurPipe } from './professeur.pipe';
+
+describe('ProfesseurPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ProfesseurPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
