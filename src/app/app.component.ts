509775d4ae47e2644d9d47c4858f51c26c6ceb54
= <=>
2022-04-04 15:47:27 +0200

    add Button search to Professeur and Matiere

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import * as firebase from 'firebase';
+//import * as firebase from 'firebase';
 import { firebaseConfig } from './app.module';
 
 @Component({
@@ -10,6 +10,6 @@ import { firebaseConfig } from './app.module';
 export class AppComponent {
   imUser = '../../../assets/images/userIm.png';
   constructor() {
-    firebase.initializeApp(firebaseConfig)
+    //firebase.initializeApp(firebaseConfig)
   }
 }
